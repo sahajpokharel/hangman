b18b2372028a57de7a4c8fb9b434343570e58628
Sahaj <sahaj.pokharel@gmail.com>
2025-06-12 18:58:11 +0545

    Enhance code and update list of words

--- hangman.cpp
+++ hangman.cpp
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
@@ -19,10 +18,11 @@ class words{
 		words()
 		{
 			srand(time(0));
-			int word_index = 1 + (rand() % 150);
+			int word_index = 1 + (rand() % 500);
 			
 			ifstream wordfile;
 			wordfile.open("words.txt");
+			
 			if(!wordfile.is_open())
 			{
 				cout<<"Sorry, unable to generate a word."<<endl;
@@ -34,91 +34,104 @@ class words{
 			while (getline(wordfile, word)) 
 			{
 		    	current_line++;
-		        if (current_line == word_index)
-		        break;
+		        
+				if (current_line == word_index)
+		        	break;
 		    }
 		    
 			wordfile.close();
 		}
 		
+		
 		void play_game()
 		{
-			char letter, letters_and_blanks[word.length()], wrong_guesses[8];
-			int wrong_attempts = 0, blanks = word.length(), i = 0;
-			bool complete;
-			
-			for(i = 0; i < word.length(); i++)
-			letters_and_blanks[i] = '_';
-			
-			while(1)
-			{
-				start_game:
-				display_hangman(wrong_attempts, letters_and_blanks, wrong_guesses);
-				cout<<endl<<endl<<"Enter letter: ";
-				
-				letter_input:
-				cin>>letter;
-				
-				if (toupper(letter) < 65 || toupper(letter) > 90)
-				{
-					system("cls");
-					goto start_game;
-				}
-				
-				int new_blanks = blanks;
-				
-				for(i = 0; i < word.length(); i++)
-				{
-					if(word[i] == toupper(letter))
-					{
-						letters_and_blanks[i] = toupper(letter);
-						blanks--;
-					}
-				}
-				
-				if(new_blanks == blanks)
-				{
-					for(i = 0; wrong_guesses[i] != '\0'; i++)
-					if(wrong_guesses[i] == letter)
-					goto blanks_continue_label;
-					
-					wrong_guesses[wrong_attempts] = letter;
-					wrong_attempts++;
-					
-					blanks_continue_label:
-					continue;
-				}
-				
-				for(i = 0; i < word.length(); i++)
-				{
-					if(letters_and_blanks[i] == '_')
-					{
-						complete = false;
-						break;
-					}
-					else
-					{
-						complete = true;
-					}
-				}
-				
-				if(complete)
-				break;
-			}
-			
-			if(complete)
-			{
-				display_hangman(wrong_attempts, letters_and_blanks, wrong_guesses);
-				cout<<endl<<endl<<"CORRECT!\n\nYou Win";
-				end_game();
-			}
-		}
+		    char letter, letters_and_blanks[word.length()], wrong_guesses[8] = {};
+		    int wrong_attempts = 0, blanks = word.length(), i = 0;
+		    bool complete;
 		
+		    for (i = 0; i < word.length(); i++)
+		        letters_and_blanks[i] = '_';
+		
+		    while (1)
+		    {
+		        bool valid_input = false;
+		        
+				while (!valid_input)
+		        {
+		            display_hangman(wrong_attempts, letters_and_blanks, wrong_guesses);
+		            cout << endl << endl << "Enter letter: ";
+		            cin >> letter;
+		            
+					if (toupper(letter) >= 65 && toupper(letter) <= 90)
+		                valid_input = true;
+		            
+					else
+		                system("cls");
+		        }
+		
+		        int new_blanks = blanks;
+		
+		        for (i = 0; i < word.length(); i++)
+		        {
+		            if (word[i] == toupper(letter))
+		            {
+		                letters_and_blanks[i] = toupper(letter);
+		                blanks--;
+		            }
+		        }
+		
+		        if (new_blanks == blanks)
+		        {
+		            bool already_guessed = false;
+		            
+					for (i = 0; wrong_guesses[i] != '\0'; i++)
+		            {
+		                if (wrong_guesses[i] == letter)
+		                {
+		                    already_guessed = true;
+		                    break;
+		                }
+		            }
+		
+		            if (!already_guessed)
+		            {
+		                wrong_guesses[wrong_attempts] = letter;
+		                wrong_attempts++;
+		            }
+		
+		            continue;
+		        }
+		
+		        complete = true;
+		        
+				for (i = 0; i < word.length(); i++)
+		        {
+		            if (letters_and_blanks[i] == '_')
+		            {
+		                complete = false;
+		                break;
+		            }
+		        }
+		
+		        if (complete)
+		            break;
+		    }
+		
+		    if (complete)
+		    {
+		        display_hangman(wrong_attempts, letters_and_blanks, wrong_guesses);
+		        cout << endl << endl << "CORRECT!\n\nYou Win";
+		        end_game();
+		    }
+		}
+
+
 		void display_hangman(int wrong_attempts, string letters_and_blanks, char wrong_guesses[])
 		{
 			if (wrong_attempts == 0)
 			{
 				system("cls");
+				
 				cout<<"\n\n\n\n\n\n\n\n\n\n";
 				cout<<letters_and_blanks;
 				cout<<endl<<endl<<"Invalid letters: ";
@@ -239,38 +252,44 @@ class words{
 				end_game();
 			}
 		}
+		
+		
 		void end_game()
 		{
 			char choice;
+			
 			cout<<endl<<endl<<"[1] Play a new game\n[2] Exit the game";
 			
-			enter_choice:
-			choice = getch();
-			
-			if(choice == '1')
+			do
 			{
-				system("cls");
-				main();
-			}
-			
-			else if(choice == '2')
-			{
-				system("cls");
+				choice = getch();
 				
-				cout<<endl<<endl<<"\t\t\tThanks for playing hangman\n\t\tVisit github.com/sahajpokharel for more games!";
+				if(choice == '1')
+				{
+					system("cls");
+					main();
+				}
 				
-				Sleep(2000);
-				exit(1);
-			}
+				else if(choice == '2')
+				{
+					system("cls");
+					
+					cout<<endl<<endl<<"\t\t\tThanks for playing hangman\n\t\tVisit github.com/sahajpokharel for more games!";
+					
+					Sleep(2000);
+					exit(1);
+				}
 			
-			else
-			goto enter_choice;
+			}while(choice!='1'||choice!='2');
 		}
 };
+
 
 int main()
 {
 	words w;
+	
 	w.play_game();
+	
 	return 0;
 }
